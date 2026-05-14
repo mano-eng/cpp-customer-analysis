b294181a271a19e2d1fd3c1a371a8f97d0a5531b
Mahmoud Ahmed <mahmoud.ahmed65@msa.edu.eg>
2026-05-14 13:12:59 +0300

    Refactor string processing in problem 083

--- tasks/arrays/problem083-append-reversed-two-letter-words.cpp
+++ tasks/arrays/problem083-append-reversed-two-letter-words.cpp
@@ -5,86 +5,59 @@ Description:
 Reads a string and appends the reversed form
 of every 2-letter word to the end of the string.
 */
-
 #include <iostream>
-
 using namespace std;
 
 void solve(char X[])
-
 {
-
     int len = 0;
 
     // calculate length
-
     while(X[len] != '\0')
-
     {
-
         len++;
-
     }
 
-    int i = 0;
+    int end = 0;
 
-    while(i < len)
-
+    while(end < len)
     {
-
-        int start = i;
+        int start = end;
 
         // move to end of word
-
-        while(X[i] != ' ' && X[i] != '\0')
-
+        while(X[end] != ' ' && X[end] != '\0')
         {
-
-            i++;
-
+            end++;
         }
 
-        int wordLen = i - start;
+        int wordLen = end - start;
 
         // if word length = 2
-
         if(wordLen == 2)
-
         {
-
             X[len] = ' ';
-
             len++;
 
             X[len] = X[start + 1];
-
             len++;
 
             X[len] = X[start];
-
             len++;
-
         }
 
-        if(X[i] != '\0')
-
+        // move after space
+        if(X[end] != '\0')
         {
-
-            i++;
-
+            end++;
         }
-
     }
 
     X[len] = '\0';
-
 }
 
 int main()
-
 {
-
-    char X[300];
+    char X[1000];
 
     gets(X);
 
@@ -93,5 +66,4 @@ int main()
     cout << X;
 
     return 0;
-
 }
